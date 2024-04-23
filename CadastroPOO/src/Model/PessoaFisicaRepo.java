8f06947de7e0467c4e827fc45dcc1bf56c6162ac
J.Gilberto <mr.joaogilberto@hotmail.com>
2024-04-23 14:46:08 -0300

    Novo comit, fazendo o Pessoa Fisica Repositorio

--- CadastroPOO/src/Model/PessoaFisicaRepo.java
+++ CadastroPOO/src/Model/PessoaFisicaRepo.java
@@ -3,6 +3,7 @@ package Model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
 
 public class PessoaFisicaRepo implements Serializable {
 
@@ -16,4 +17,48 @@ public class PessoaFisicaRepo implements Serializable {
         listaPessoaFisicas.add(pessoaFisica);
     }
 
+    public void alterar(PessoaFisica pessoaFisica) {
+        for (int lFisica = 0; lFisica < listaPessoaFisicas.size(); lFisica++) {
+            PessoaFisica pFisica = listaPessoaFisicas.get(lFisica);
+            if (pFisica.getId() == pessoaFisica.getId()) {
+                listaPessoaFisicas.set(lFisica, pessoaFisica);
+                break;
+            }
+        }
+    }
+
+    public void excluir(int id) {
+        for (int lFisica = 0; lFisica < listaPessoaFisicas.size(); lFisica++) {
+            PessoaFisica pFisica = listaPessoaFisicas.get(lFisica);
+            if (pFisica.getId() == id) {
+                listaPessoaFisicas.remove(lFisica);
+                break;
+            }
+        }
+    }
+
+    public PessoaFisica obter(int id) {
+        for (PessoaFisica pFisica : listaPessoaFisicas) {
+            if (pFisica.getId() == id) {
+                return pFisica;
+            }
+        }
+        return null;
+    }
+
+    public List<PessoaFisica> obterTodos() {
+        return listaPessoaFisicas;
+    }
+
+    public void persistir(String arquivoNome) throws IOException {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(arquivoNome))) {
+            outputStream.writeObject(listaPessoaFisicas);
+        }
+    }
+
+    public void recuperar(String arquivoNome) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(arquivoNome))) {
+            listaPessoaFisicas = (List<PessoaFisica>) inputStream.readObject();
+        }
+    }
 }
