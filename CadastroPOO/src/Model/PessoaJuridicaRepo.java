2c67512532b668a5f421173e3b03fd5f9e9605a9
J.Gilberto <mr.joaogilberto@hotmail.com>
2024-04-25 15:46:31 -0300

    Falta agora so fazer o cadastro e pronto

--- CadastroPOO/src/Model/PessoaJuridicaRepo.java
+++ CadastroPOO/src/Model/PessoaJuridicaRepo.java
@@ -26,7 +26,38 @@ public class PessoaJuridicaRepo implements Serializable {
     }
 
     public void excluir(int id) {
-
+        for (int lJuridica = 0; lJuridica < listaPessoaJuridicas.size(); lJuridica++) {
+            PessoaJuridica pJuridica = listaPessoaJuridicas.get(lJuridica);
+            if (pJuridica.getId() == id) {
+                listaPessoaJuridicas.remove(lJuridica);
+                break;
+            }
+        }
     }
 
+    public PessoaJuridica obter(int id) {
+        for (PessoaJuridica pJuridica : listaPessoaJuridicas) {
+            if (pJuridica.getId() == id) {
+                return pJuridica;
+            }
+        }
+        return null;
+    }
+
+    public List<PessoaJuridica> obterTodos() {
+        return listaPessoaJuridicas;
+    }
+
+    public void persistir(String arquivoNome) throws IOException {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(arquivoNome))) {
+            outputStream.writeObject(listaPessoaJuridicas);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void recuperar(String arquivoNome) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(arquivoNome))) {
+            listaPessoaJuridicas = (List<PessoaJuridica>) inputStream.readObject();
+        }
+    }
 }
