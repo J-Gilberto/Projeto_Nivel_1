b6ede22b4369e0ca5d754f2981d90f16f06fa99f
J.Gilberto <mr.joaogilberto@hotmail.com>
2024-04-26 11:16:14 -0300

    Pronto, dando inicio ao CadastroPOO

---
+++ CadastroPOO/src/CadastroPoo/CadastroPOO.java
@@ -0,0 +1,17 @@
+package CadastroPoo;
+
+import java.io.IOException;
+import Model.PessoaFisica;
+import Model.PessoaFisicaRepo;
+import Model.PessoaJuridica;
+import Model.PessoaJuridicaRepo;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CadastroPOO {
+
+    private static final Scanner scan = new Scanner(System.in);
+    private static final PessoaFisicaRepo rPessoaFisica = new PessoaFisicaRepo();
+    private static final PessoaJuridicaRepo rPessoaJuridica = new PessoaJuridicaRepo();
+
+}
